933f3354f76799af1c5ad85084d2390c92170808
codeandlove <codeandlove@gmail.com>
2022-02-17 21:01:34 +0100

    updates

--- src/firebase.js
+++ src/firebase.js
@@ -8,18 +8,14 @@ import credentials from './credentials';
 
 // Initialize Firebase
 const app = firebase.initializeApp(credentials);
+//
+const appCheck = firebase.appCheck();
 
-if(process.env.NODE_ENV === 'production') {
-    const appCheck = firebase.appCheck();
-
-    // Pass your reCAPTCHA v3 site key (public key) to activate(). Make sure this
-    // key is the counterpart to the secret key you set in the Firebase console.
-    appCheck.activate(process.env.REACT_APP_RECAPTCHA_API_V3, true);
-
-}
+// Pass your reCAPTCHA v3 site key (public key) to activate(). Make sure this
+// key is the counterpart to the secret key you set in the Firebase console.
+appCheck.activate(process.env.REACT_APP_RECAPTCHA_API_V3, true)
 
 export const analytics = firebase.analytics(app);
 export const auth = firebase.auth();
 
 export default firebase;
-
