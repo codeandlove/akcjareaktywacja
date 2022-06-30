ce9d20e2b209726057c948151f23ef1ddb98dc1f
codeandlove <codeandlove@gmail.com>
2022-06-30 20:40:44 +0200

    Updates: added Categories to events

--- src/client/containers/CategorySelector/CategorySelector.js
+++ src/client/containers/CategorySelector/CategorySelector.js
@@ -1,7 +1,8 @@
 import React, {useEffect, useState} from 'react';
-import {buildCategories} from "../../utils";
+import {buildCategories, getCategoriesDataByIds} from "../../utils";
 import './CategorySelector.scss';
 import {Image, Icon, List, Transition} from "semantic-ui-react";
+import {analytics} from "../../../firebase/analytics";
 
 const CategorySelector = (props) => {
     const {onChosenCategory, selectedCategories} = props;
