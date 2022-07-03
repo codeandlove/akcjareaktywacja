dc42225d6b35800a0030de6d5813327f79520d1b
codeandlove <codeandlove@gmail.com>
2022-07-03 17:30:06 +0200

    Updates: refactoring

--- src/client/components/Avatar/Avatar.js
+++ src/client/components/Avatar/Avatar.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {Image} from "semantic-ui-react";
 
 import avatarPlaceholder from "./../../../assets/profile_avatar.png";
+import PropTypes from "prop-types";
 import {compose} from "redux";
 import {firebaseConnect} from "react-redux-firebase";
 import {connect} from "react-redux";
@@ -12,6 +13,10 @@ const Avatar = ({auth, profile, size}) => {
     return (
         <Image src={avatarImage || auth.photoURL || avatarPlaceholder} size={size || 'mini'} avatar />
     )
+}
+
+Avatar.propTypes = {
+    firebase: PropTypes.object.isRequired
 }
 
 const enhance = compose(
