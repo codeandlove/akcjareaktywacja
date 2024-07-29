0744d0dfc9d15b496d2329ba5c4c9d089fc09183
Piotr Makowski <codeandlove@gmail.com>
2024-07-29 17:54:02 +0200

    Update credentials.js

--- src/firebase/credentials.js
+++ src/firebase/credentials.js
@@ -1,23 +1,23 @@
 export const dev = {
     apiKey: process.env.REACT_APP_FIREBASEAPI_KEY,
-    authDomain: "akcjareaktywacja-dev.firebaseapp.com",
-    databaseURL: "https://akcjareaktywacja-dev-default-rtdb.firebaseio.com",
-    projectId: "akcjareaktywacja-dev",
-    storageBucket: "akcjareaktywacja-dev.appspot.com",
-    messagingSenderId: "717290649510",
-    appId: "1:717290649510:web:c4f6c198254b43b4aa0167",
-    measurementId: "G-T025PTD7Y0"
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
 }
 
 export const prod = {
     apiKey: process.env.REACT_APP_FIREBASEAPI_KEY,
-    authDomain: "akcjareaktywacja-39acb.firebaseapp.com",
-    databaseURL: "https://akcjareaktywacja-39acb.firebaseio.com",
-    projectId: "akcjareaktywacja-39acb",
-    storageBucket: "akcjareaktywacja-39acb.appspot.com",
-    messagingSenderId: "951885335245",
-    appId: "1:951885335245:web:52b54d3238fa27e6",
-    measurementId: "G-8JY9X653ZY"
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
 }
 
 export default process.env.NODE_ENV === 'development' ? dev : prod;
